c402bc548f22c7509c899fecb22259a129fe5180
Doan Nhat Anh <nhatanhdoan1505@gmail.com>
2020-12-21 09:46:00 +0700

    Fix

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Express EJS Template
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// Static foler
+app.use(express.static(__dirname + '/public'));
+
+
 // Express body-Parser
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
 
-
-const PORT = process.env.PORT || 5000;
-
+app.use('/', require('./routes/index'));
 app.use('/search', require('./routes/search'));
 app.use('/link', require("./routes/getLink"));
 app.use('/chapter', require("./routes/getChapter"));
 app.use('/image', require("./routes/getImages"));
 
-app.listen(PORT, console.log("Server is starting"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
